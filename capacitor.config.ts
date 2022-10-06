6d0c53c4e8f9e26b7f241b1ef5f0f934dce46dbc
Chepkorir Brenda <40006302+peta-byte@users.noreply.github.com>
2022-10-06 12:15:17 -0500

    update (#1)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ng-ionic-basics',
+  appName: 'dynamic debate',
   webDir: 'www',
   bundledWebRuntime: false
 };
