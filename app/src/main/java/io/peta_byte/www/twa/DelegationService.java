6570780b46b83e6499ea2f656cd04b321d5e1817
peta-byte <40006302+peta-byte@users.noreply.github.com>
2022-10-06 21:33:59 -0500

    android

---
+++ app/src/main/java/io/peta_byte/www/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.peta_byte.www.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
